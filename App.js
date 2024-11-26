f4ba4d839cf0a2e4079f11fef21ff1728067cb26
karthikeyan <129775960+KarthikeyanR007@users.noreply.github.com>
2024-11-26 16:58:39 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,15 +5,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ActivityIndicator, View } from 'react-native';
 import { AuthProvider, useAuth } from './AuthContext';
-// import { useDispatch } from 'react-redux';
-// import { setUser } from './redux/userSlice';
+
 import Home from './src/components/Screen/Home';
 import Login from './src/Auth/Login';
 import SignUp from './src/Auth/SignUp';
-// import Expenses from './src/components/Screen/Expenses';
-// import DayExpense from './src/components/Screen/DayExpense';
-// import Settings from './src/components/Screen/Settings';
-// import ProfileEdit from './src/components/Screen/ProfileEdit';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from './src/Redux/userSlice';
 import About from './src/components/Screen/About';
@@ -78,86 +74,3 @@ const App = () => (
 export default App;
 
 
-// import './gesture-handler';
-// import '@react-native-firebase/app';
-// import React, { useState, useEffect } from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { ActivityIndicator, View } from 'react-native';
-// import { AuthProvider, useAuth } from './AuthContext';
-// import { useDispatch } from 'react-redux';
-// import { setUser } from './src/Redux/userSlice';
-// import Home from './src/components/Screen/Home';
-// import Login from './src/Auth/Login';
-// import SignUp from './src/Auth/SignUp';
-// import Expenses from './src/components/Screen/Expenses';
-// import DayExpense from './src/components/Screen/DayExpense';
-// import Settings from './src/components/Screen/Settings';
-// import ProfileEdit from './src/components/Screen/ProfileEdit';
-// import AboutScreen from './src/components/Screen/About';
-
-// // GlueStack UI imports
-// import { UIProvider, ThemeProvider } from 'gluestack-ui';
-// import { theme } from '@gluestack-ui/themed'; // Default GlueStack theme
-// import { SafeAreaView } from 'react-native';
-
-// const Stack = createStackNavigator();
-
-// const MainStack = () => (
-//   <Stack.Navigator>
-//     <Stack.Screen name="AboutScreen" component={AboutScreen} options={{ headerShown: false }} />
-//     <Stack.Screen name="Expenses" component={Expenses} options={{ headerShown: false }} />
-//     <Stack.Screen name="DayExpense" component={DayExpense} options={{ headerShown: false }} />
-//     <Stack.Screen name="Settings" component={Settings} options={{ headerShown: false }} />
-//     <Stack.Screen name="ProfileEdit" component={ProfileEdit} options={{ headerShown: false }} />
-//   </Stack.Navigator>
-// );
-
-// const AuthStack = () => (
-//   <Stack.Navigator screenOptions={{ headerShown: false }}>
-//     <Stack.Screen name="Login" component={Login} />
-//     <Stack.Screen name="SignUp" component={SignUp} />
-//   </Stack.Navigator>
-// );
-
-// const AppContent = () => {
-//   const [isLoading, setIsLoading] = useState(true);
-//   const { user } = useAuth();
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     setIsLoading(false);
-//   }, []);
-
-//   if (!isLoading) {
-//     user ? dispatch(setUser(user.email)) : null;
-//   }
-
-//   if (isLoading) {
-//     return (
-//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//         <ActivityIndicator size="large" />
-//       </View>
-//     );
-//   }
-
-//   return (
-//     <NavigationContainer>
-//       {user ? <MainStack /> : <AuthStack />}
-//     </NavigationContainer>
-//   );
-// };
-
-// const App = () => (
-//   <AuthProvider>
-//     <UIProvider>
-//       <ThemeProvider theme={theme}>
-//         <SafeAreaView style={{ flex: 1 }}>
-//           <AppContent />
-//         </SafeAreaView>
-//       </ThemeProvider>
-//     </UIProvider>
-//   </AuthProvider>
-// );
-
-// export default App;
